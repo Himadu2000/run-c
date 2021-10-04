5e474fd9a34ff9958dd5cd9b9e658f2b620a910e
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2021-10-04 08:07:57 +0530

    First

---
+++ index.js
@@ -0,0 +1,20 @@
+const CronJob = require("cron").CronJob;
+const { exec } = require("child_process");
+const job = new CronJob(
+  "*/1 11-14 * * 1-5",
+  () => {
+    if (job.lastDate()?.getHours() === 2) process.exit(0);
+    if (
+      job.lastDate()?.getHours() === 1 &&
+      job.lastDate()?.getMinutes() === 50
+    ) {
+      console.log("last 10");
+    }
+    console.log("Running every minute.");
+    exec("yarn start");
+  },
+  null,
+  true,
+  "Asia/Colombo"
+);
+job.start();
